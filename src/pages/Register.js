71bcc02ace97322db7ed9eec50c614db5dbfde84
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-07 17:28:18 +0400

    Register Page Setup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { Logo } from "../components";
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 function Register() {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChage = (e) => {
+    console.log(e.target);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name field */}
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChage}
+          className="form-input"
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 }
 
 export default Register;
