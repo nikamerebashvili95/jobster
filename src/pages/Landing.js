6180a7a2cdbeb97584b1bcbfc559c5d5ed24ec2a
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-07 15:26:42 +0400

    add Landing Page Structure

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,28 @@ import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 function Landing() {
-  return <h1>Landing Page</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
+            architecto optio harum eius quibusdam ullam cumque, voluptates
+            doloribus perspiciatis dolores!
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 }
 
 export default Landing;
