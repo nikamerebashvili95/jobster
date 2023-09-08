479e17b205283b5e54a6baa89f363bc6ffccc648
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-08 14:25:57 +0400

    work with handleChange and Empty values

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -12,11 +12,16 @@ function Register() {
   const [values, setValues] = useState(initialState);
 
   const handleChage = (e) => {
-    console.log(e.target);
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
   };
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      console.log("Please fill out all feilds");
+    }
   };
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
