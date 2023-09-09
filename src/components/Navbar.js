ca1164cb2aed5f04718b42f1dae92e26e3f072f3
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-09 15:01:40 +0400

    work with Toggle Sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,19 @@ import { useDispatch, useSelector } from "react-redux";
 import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 import Logo from "./Logo";
+import { toggleSidebar } from "../features/user/userSlice";
 function Navbar() {
   const { user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
   return (
     <Wrapper>
       <div className="nav-center">
-        <button
-          type="button"
-          className="toggle-btn"
-          onClick={() => console.log("toggle sidebar")}
-        >
+        <button type="button" className="toggle-btn" onClick={toggle}>
           <FaAlignLeft />
         </button>
         <div>
