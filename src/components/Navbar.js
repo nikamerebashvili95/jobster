0409ebc6d66e47e273614076f5855fa3335204eb
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-12 14:47:54 +0400

    add: Logout Message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ function Navbar() {
             <button
               type="button"
               className="dropdown-btn"
-              onClick={() => dispatch(logoutUser())}
+              onClick={() => dispatch(logoutUser("Logging out.."))}
             >
               logout
             </button>
