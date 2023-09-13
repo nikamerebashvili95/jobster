6e86a7f51db98999c72c42fe21fe1d46c984c1f8
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-13 15:59:30 +0400

    work with Set Edit Job

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -72,6 +72,9 @@ const jobSlice = createSlice({
         jobLocation: getUserFromLocalStorage()?.location || "",
       };
     },
+    setEditJob: (state, { payload }) => {
+      return { ...state, isEditing: true, ...payload };
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -85,6 +88,6 @@ const jobSlice = createSlice({
   },
 });
 
-export const { handleChange, clearValues } = jobSlice.actions;
+export const { handleChange, clearValues, setEditJob } = jobSlice.actions;
 
 export default jobSlice.reducer;
