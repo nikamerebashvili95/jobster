139247d53b9cc21f332516c19fd55baa3e0b9393
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-14 13:55:28 +0400

    Refactor App

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -27,11 +27,7 @@ export const getAllJobs = createAsyncThunk(
     let url = `/jobs`;
 
     try {
-      const resp = await customFetch.get(url, {
-        headers: {
-          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-        },
-      });
+      const resp = await customFetch.get(url);
       console.log(resp.data);
       return resp.data;
     } catch (error) {
