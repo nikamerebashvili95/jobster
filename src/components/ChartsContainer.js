fea900e2fd4d39afd8055c147c97694c2f3de181
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-14 15:10:12 +0400

    add AreaChart and BarChart and work with Chars Container

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,5 +1,21 @@
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+
 function ChartsContainer() {
-  return <div>ChartsContainer</div>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useSelector((store) => store.allJobs);
+  return (
+    <Wrapper>
+      <h4>Mothly Aplications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 }
 
-export default ChartsContainer
+export default ChartsContainer;
