38016fc124e3b28f3d739355331450ea4e48830d
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-09-15 20:50:18 +0400

    work with Query String Params

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -6,13 +6,21 @@ import Loading from "./Loading";
 import { getAllJobs } from "../features/allJobs/allJobsSlice";
 import PageBtnContainer from "./PageBtnContainer";
 function JobsContainer() {
-  const { jobs, isLoading, page, totalJobs, numOfPages } = useSelector(
-    (store) => store.allJobs
-  );
+  const {
+    jobs,
+    isLoading,
+    page,
+    totalJobs,
+    numOfPages,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+  } = useSelector((store) => store.allJobs);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllJobs());
-  }, []);
+  }, [page, search, searchStatus, searchType, sort]);
   if (isLoading) {
     return <Loading center />;
   }
